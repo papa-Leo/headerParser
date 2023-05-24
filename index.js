849cb4595a0e50e2ca97698e17d2f912390d414f
papa-Leo <leandroconsani21@gmail.com>
2023-05-24 23:49:13 +0200

    changed express config to find client ip

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 
 app.use(express.static('public'));
 
+// required to find client ip
+app.set('trust proxy', true)
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
