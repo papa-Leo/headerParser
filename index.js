837d1526e26917142ddf78b7ee62279624a2b1ae
papa-Leo <leandroconsani21@gmail.com>
2023-05-24 23:45:21 +0200

    bulk coding done

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// index.js
-// where your node app starts
-
 // init project
 require('dotenv').config();
 var express = require('express');
@@ -11,18 +8,23 @@ var app = express();
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/whoami', (req, res) => {
+	const ip = req.ip;
+	const lang = req.headers['accept-language'];
+	const software = req.headers['user-agent'];
+
+	res.json({
+		'ipaddress': ip,
+		'language': lang,
+		'software': software
+	});
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
